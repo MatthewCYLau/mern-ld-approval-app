b752f40a4379017359059c52ca76bdc12850dbc7
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-02-09 19:17:52 +0000

    added new Login css

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/auth";
 import { Form, Button } from "react-bootstrap";
+import "./Login.css";
 
 const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
