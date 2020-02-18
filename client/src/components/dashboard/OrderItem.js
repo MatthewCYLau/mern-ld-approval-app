d4d97ac36a915beee767918d02f0f61d1746b2bb
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-02-18 21:07:43 +0000

    improved ui for order item

--- client/src/components/dashboard/OrderItem.js
+++ client/src/components/dashboard/OrderItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
-import { ListGroup, Button } from "react-bootstrap";
 import { connect } from "react-redux";
 import { addLike, removeLike } from "../../actions/course";
 import { approveOrder } from "../../actions/order";
 import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
@@ -13,12 +13,13 @@ import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Hidden from "@material-ui/core/Hidden";
 
 const leverageStyles = makeStyles({
-  cardActionArea: {
+  gridItem: {
     "& a": { textDecoration: "none" }
+  },
+  approveButton: {
+    margin: "0 1rem"
   }
 });
 
@@ -29,58 +30,48 @@ const OrderItem = ({
 }) => {
   const classes = leverageStyles();
   return (
-    <Grid item className={classes.cardActionArea}>
-      <CardActionArea href={course.url}>
-        <Card className={classes.card}>
-          <div className={classes.cardDetails}>
-            <CardContent>
-              <Typography component="h2" variant="h5">
-                {course.name}
-              </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                {course.provider}
-              </Typography>
-              <Typography variant="subtitle1" paragraph>
-                {course.price}
-              </Typography>
-              {approved ? (
-                <Chip color="primary" label="Approved" />
-              ) : (
-                <Chip label="Pending" />
-              )}
-            </CardContent>
-          </div>
-        </Card>
-      </CardActionArea>
+    <Grid item className={classes.gridItem}>
+      <Card variant="outlined">
+        <CardActionArea href={course.url}>
+          <CardContent>
+            <Typography component="h2" variant="h5">
+              {course.name}
+            </Typography>
+            <Typography variant="subtitle1">
+              Provider: {course.provider}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Price: {course.price}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Applied on <Moment format="DD/MM/YYYY">{date}</Moment>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardContent>
+          {isAdmin && (
+            <Typography variant="subtitle1" paragraph color="textSecondary">
+              Applied by: {user.name}
+            </Typography>
+          )}
+          {approved ? (
+            <Chip color="primary" label="Approved" />
+          ) : (
+            <Chip label="Pending" />
+          )}
+          {isAdmin && !approved && (
+            <Button
+              variant="outlined"
+              color="primary"
+              className={classes.approveButton}
+              onClick={() => approveOrder(_id)}
+            >
+              Approve
+            </Button>
+          )}
+        </CardContent>
+      </Card>
     </Grid>
-
-    // <div>
-    //   <ListGroup.Item>
-    //     <a className="course-name" href={course.url}>
-    //       <h4>{course.name}</h4>
-    //     </a>
-    //     <p>Provider: {course.provider}</p>
-    //     <p>Price: {course.price}</p>
-    //     <p>
-    //       Applied on <Moment format="DD/MM/YYYY">{date}</Moment>
-    //     </p>
-    //     {isAdmin && <p>{user.name}</p>}
-    //     {approved ? (
-    //       <Chip color="primary" label="Approved" />
-    //     ) : (
-    //       <Chip label="Pending" />
-    //     )}
-    //     {isAdmin && (
-    //       <Button
-    //         className="action-btn"
-    //         variant="success"
-    //         onClick={() => approveOrder(_id)}
-    //       >
-    //         Approve
-    //       </Button>
-    //     )}
-    //   </ListGroup.Item>
-    // </div>
   );
 };
 
