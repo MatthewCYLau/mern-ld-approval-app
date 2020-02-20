df79b0453cb710bc121a93a6275541cc0c5f82e3
Gabby Sanghera <gsanghera@uk-c02zw0h2lvdm.uk.deloitte.com>
2020-02-20 20:21:27 +0000

    delete button classes and moved to pending

--- client/src/components/dashboard/OrderItem.js
+++ client/src/components/dashboard/OrderItem.js
@@ -73,9 +73,23 @@ const OrderItem = ({
           </Button>
           </Grid>
           </Grid>
-            </div>) : (
+            </div>) : (<div>
+              <Grid container spacing={2} justify= "flex-start">
+              <Grid item>
             <Chip label="Pending" />
-          )}
+            </Grid>
+            <Grid item>
+            <Button
+            variant="contained"
+            color="secondary"
+            className={(classes.deleteButton)}
+            onClick={() => deleteOrder(_id)}
+          >
+            Delete 
+          </Button>
+          </Grid>
+          </Grid>
+            </div>)}
           {isAdmin && !approved && (
             
             <Button
