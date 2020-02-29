a668135614441d7e803c444320522bda7a96dacc
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-02-29 11:05:30 +0000

    Usd Intro component for Admin panel

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -5,6 +5,8 @@ import OrderItem from "../dashboard/OrderItem";
 import { getOrders } from "../../actions/order";
 import { ListGroup } from "react-bootstrap";
 import Spinner from "../layout/Spinner";
+import Intro from "../layout/Intro";
+
 
 const Admin = ({ getOrders, order: { orders, loading }, auth: { user } }) => {
   useEffect(() => {
@@ -15,13 +17,8 @@ const Admin = ({ getOrders, order: { orders, loading }, auth: { user } }) => {
   ) : (
     <Fragment>
       <div className="container">
-        <h1 className="large text-primary">Admin</h1>
-        <p className="lead">
-          <i className="fas fa-user" /> Welcome Admin {user && user.name}
-        </p>
-        <p>
-          Here are the all orders across the portal, and their approval status.
-        </p>
+        <Intro componentName="Admin" username={user && user.name} componentHeadline="Here are the all orders across the portal, and their approval status"/>
+
         <ListGroup>
           {orders.map(order => (
             <OrderItem key={order._id} order={order} isAdmin={true} />
